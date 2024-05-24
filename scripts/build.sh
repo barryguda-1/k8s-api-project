523023f30d458c58a0ca82ac4c3085c1ac49a950
Barry Guda <barryguda@Barrys-MacBook-Pro.local>
2024-05-24 14:57:23 +0200

    pipeline configs

---
+++ scripts/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+echo "docker installation started........."
+curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+echo "docker installation done"
+sudo chmod +x /usr/local/bin/docker-compose
+docker ps
+echo "docker installation done........."
+DOCKER_BUILD_ARGS="-t k8s-service:v1" 
+echo $DOCKER_BUILD_ARGS
+docker build $DOCKER_BUILD_ARGS . 
+echo "docker build done........."
+
+docker login -u iamapikey --password-stdin "icr.io" <<< "$(get_env icr_push_apikey)"
+docker tag k8s-service:v1 icr.io/namespace/k8s-service:v1
+docker images
+docker push icr.io/namespace/k8s-service:v1
+digest=$(docker inspect --format='{{index .RepoDigests 0}}' icr.io/namespace/k8s-service:v1 | cut -d@ -f2)
+echo "saving artifact"
+save_artifact ui_service "name=icr.io/k8s-service:v1"
+save_artifact ui_service "type=image"
+save_artifact ui_service "digest=${digest}"
+save_artifact ui_service "image_type=nonprod"
+save_artifact ui_service "twistlock_group=$(get_env twistlock-group-id)"
---
+++ scripts/code_setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# shellcheck source=/dev/null
+source "${ONE_PIPELINE_PATH}"/tools/get_repo_params
+APP_TOKEN_PATH="./app-token"
+read -r APP_REPO_NAME APP_REPO_OWNER APP_SCM_TYPE APP_API_URL < <(get_repo_params "$(load_repo app-repo url)" "$APP_TOKEN_PATH")
+
+if [[ $APP_SCM_TYPE == "gitlab" ]]; then
+  # shellcheck disable=SC2086
+  curl --location --request PUT "${APP_API_URL}/projects/$(echo ${APP_REPO_OWNER}/${APP_REPO_NAME} | jq -rR @uri)" \
+    --header "PRIVATE-TOKEN: $(cat $APP_TOKEN_PATH)" \
+    --header 'Content-Type: application/json' \
+    --data-raw '{
+    "only_allow_merge_if_pipeline_succeeds": true
+    }'
+else
+  # If PR, then target branch of the PR is the branch to protect
+  branch=$(get_env base-branch "")
+  if [ -z "$branch" ]; then
+    branch="$(cat /config/git-branch)"
+  fi
+  curl -H "Authorization: Bearer $(cat "${APP_TOKEN_PATH}")" "${APP_API_URL}/repos/${APP_REPO_OWNER}/${APP_REPO_NAME}/branches/$branch/protection" \
+    -XPUT -d '{"required_pull_request_reviews":{"dismiss_stale_reviews":true},"required_status_checks":null,"enforce_admins":null,"restrictions":null}'
+fi
